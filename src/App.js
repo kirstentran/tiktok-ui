78c28b4d3a0c16e3142ec69331bac026a8f90c0e
Kirsten Tran <uyen.tran@usm.edu>
2024-04-12 01:25:30 -0500

    video 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
+import'./components/Button'
 import './App.css';
+import Button from '~/components/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+      <Button/>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
