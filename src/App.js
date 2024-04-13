e2426aaace5a930ed39afdd8b2f3d59dcbba7b1f
Kirsten Tran <uyen.tran@usm.edu>
2024-04-12 22:39:48 -0500

    vid 5

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import logo from './logo.svg';
-import'./components/Button'
 import './App.css';
 import Button from '~/components/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      <Button/>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <Button />
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
