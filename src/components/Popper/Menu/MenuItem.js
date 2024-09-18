9f1d0b1c11291b707c039cdc5e2d92758d0f330c
Kirsten Tran <uyen.tran@usm.edu>
2024-09-18 00:19:54 -0500

    13:42

--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -21,5 +21,8 @@ function MenuItem({ data, onClick }) {
     </Button>
   );
 }
-MenuItem.propTypes = {};
+MenuItem.propTypes = {
+  data: PropTypes.object.isRequired,
+  onClick: PropTypes.func,
+};
 export default MenuItem;
